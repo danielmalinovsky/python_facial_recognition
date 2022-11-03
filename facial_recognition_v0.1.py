d8a628b37ea706160ad1c6c77ab23e43c8b38a08
danielmalinovsky <106654319+danielmalinovsky@users.noreply.github.com>
2022-11-03 18:32:53 +0100

    Sprint 2 commit
    New functions for bbox detection and verification are in src file
    Train test split implemented
    resizing implemented within face_crop function

--- facial_recognition_v0.1.py
+++ facial_recognition_v0.1.py
@@ -2,24 +2,32 @@
 import cv2
 from matplotlib import pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+import random
+import os
+#import cvlib as cv
 
 # User defined functions
 import src.proprietary_functions as src
-
 
 """Image dataset source: https://mmlab.ie.cuhk.edu.hk/projects/CelebA.html"""
 
 
 # %%
 # Setting filepaths
-
 image_file = './sample_data/Img/'
 annotation_file = './sample_data/Anno/'
+export_file = './export'
 
 identity_file = annotation_file + 'identity_CelebA.txt'
 bbox_file = annotation_file + 'list_bbox_celeba.txt'
-image_list = ['000001.jpg', '000002.jpg']
 
+# Train/test split variables
+random_seed = 123
+test_size = 0.2
+validation_size = 0.2
+
+# Setting column names
 image_id_col = 'image_id'
 bbox_col_names = {
     'x_start' : 'x_1',
@@ -29,111 +37,155 @@ bbox_col_names = {
     'x_end' : '',
     'y_end' : ''}
 
+identity_file = annotation_file + 'identity_CelebA.txt'
+bbox_file = annotation_file + 'list_bbox_celeba.txt'
+
+
+
 # Loading dataset metadata
 identity = pd.read_csv(identity_file, sep=" ", header = None,names=['image', 'image_id'])
 bbox = pd.read_csv(bbox_file, delim_whitespace=True)
 
-# %%
-# Cropping images
 
-crop_img = src.face_crop(image_list[0], image_file, image_id_col, bbox_col_names, bbox, False)
-plt.imshow(cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB))
+#%% Filtering faces that appear at least 20 times
+labels_annot = pd.DataFrame(identity.image_id.value_counts(ascending=True)).query('image_id > 20').index.tolist()
+identity_filtered = identity[identity['image_id'].isin(labels_annot)]
 
-# %%
-#Resizing cropped image
-resize_crop_img = cv2.resize(crop_img, (300, 300))
-plt.imshow(cv2.cvtColor(resize_crop_img, cv2.COLOR_BGR2RGB))
-
-# %%
-#reading the annotations
-from sklearn.model_selection import train_test_split
-annot_path = str(input()) #enter your path.
-# C:/Users/ngnpe/OneDrive/Desktop/Agile_ML_zip/Anno/
-
-#%%
-annots = pd.read_csv(annot_path +'identity_CelebA.txt',
-                        delim_whitespace = True,
-                        header = None,
-                        names = ['jpg', 'label'])
-
-#filtering lables having at least 3 observations.
-labels_annot = pd.DataFrame(annots.label.value_counts(ascending=True)).query('label > 20').index.tolist()
-annots_filtered = annots[annots['label'].isin(labels_annot)]
-#Splitting the annotations
-
-imgs = annots_filtered['jpg']
-labels = annots_filtered['label']
+#%% [SPRINT 2] Train/test split of the annotations
+imgs = identity_filtered['image']
+labels = identity_filtered['image_id']
 
 temp_imgs, test_imgs, _, __ = train_test_split(imgs, labels,
-                                               test_size = 0.2,
-                                               random_state = 123,        
+                                               test_size = test_size,
+                                               random_state = random_seed,        
                                                stratify = labels)
 train_imgs, valid_imgs, _, __ = train_test_split(temp_imgs, _,
-                                               test_size = 0.25,
-                                               random_state = 123,        
+                                               test_size = validation_size/(1-test_size),
+                                               random_state = random_seed,        
                                                stratify = _)
 
-# %%
-print('Export the files?')
-if input() == 'Yes':
-    train_imgs.to_csv('train_imgs.csv', index = False)
-    valid_imgs.to_csv('valid_imgs.csv', index = False)
-    test_imgs.to_csv('test_imgs.csv', index = False)
-    
-# %%
-#10 random pictures
-import random
-random.seed(123)
-random_pics = random.choices(annots_filtered['jpg'].values, k=10)
-bbox_filtered = bbox[bbox['image_id'].isin(random_pics)]
+#%% 
+# Safe train/test split
+
+if not os.path.exists(export_file):
+    os.makedirs(export_file)
+
+if not os.path.exists(export_file + '/setting'):
+    os.makedirs(export_file + '/setting')
+
+if export_file != '':
+    train_imgs.to_csv(export_file + '/setting/train_imgs.csv', index = False)
+    valid_imgs.to_csv(export_file + '/setting/valid_imgs.csv', index = False)
+    test_imgs.to_csv(export_file + '/setting/test_imgs.csv', index = False)
 
 #%%
-#path of images
-imgs_path = str(input()) #enter your path
-#C:/Users/ngnpe/OneDrive/Desktop/Agile_ML_zip/img_celeba.7z/img_celeba_001/
+# [SPRINT 2] Random selection
+random.seed(random_seed)
+random_pics = random.choices(identity_filtered['image'].values, k=10)
+bbox_filtered = bbox[bbox['image_id'].isin(random_pics)] #generate bboxes
+
+# %% [SPRINT 2] BBox engine demonstration
+
+img = cv2.imread(image_file+random_pics[0])
+bbox_generated = src.bbox_engine(img, method = 0)
+
+cv2.rectangle(img, (bbox_generated['x_1'],bbox_generated['y_1']), (bbox_generated['x_end'],bbox_generated['y_end']), (0,255,0), 2)       
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
 # %%
-#Plotting 10 random pictures
+# [SPRINT 2 demonstration]
+# %% Generating Bboxes
+
+bbox_generated = pd.DataFrame(columns= ['image_id', 'x_1', 'y_1', 'width', 'height', 'x_end', 'y_end'])
+
+for pic in random_pics:
+
+    img = cv2.imread(image_file+pic)
+    bbox_coordinates = src.bbox_engine(img, method= 1)
+    bbox_coordinates['image_id'] = pic
+    bbox_generated = bbox_generated.append(bbox_coordinates, ignore_index = True)
+
+
+# %% Bbox verification
+
+image_lens = pd.DataFrame(columns= ['image_id', 'len'])
+
+for pic, in zip(random_pics):
+
+    image_path = image_file + pic
+    img = cv2.imread(image_path)
+    len_dict = {'image_id' : pic, 'len' : len(img)}
+    image_lens = image_lens.append(len_dict, ignore_index = True)
+
+bbox_verification_df = src.bbox_verification(bbox_filtered, bbox_generated, image_lens)
+
+# %% Large bbox mismatch plot
+large_diff_images = bbox_verification_df[bbox_verification_df['relative_diff'] > 0.5]['image_id']
+
+for i in range(len(large_diff_images)):
+    src.bbox_show_compare(large_diff_images.iloc[i], image_file, 'image_id', bbox_col_names, bbox_filtered, bbox_generated)
+
+#%%
+# [SPRINT 2] Plotting 10 random pictures
 fig_1, axs_1 = plt.subplots(nrows = 5, ncols = 2, figsize = (15, 35))
 
 for pic, ax_1 in zip(random_pics, axs_1.ravel()):
 
-    image_path = imgs_path + pic
+    image_path = image_file + pic
     img = cv2.imread(image_path)
 
     ax_1.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    ax_1.title.set_text(pic)
 
 fig_1.tight_layout()
 plt.show()
 
 #%%
-#Cropping 10 random pictures
+# [SPRINT 2] Generating bboxes for 10 random pictures
 fig_2, axs_2 = plt.subplots(nrows = 5, ncols = 2, figsize = (15, 35))
 
 for pic, ax_2 in zip(random_pics, axs_2.ravel()):
 
-    crop_img = src.face_crop(pic, imgs_path, 'image_id',
-                             bbox_col_names, bbox_filtered,
-                             show_bbox = False)
+    crop_img = src.face_crop(pic, image_file, 'image_id',
+                             bbox_col_names, bbox_generated,
+                             show_bbox = True)
 
     ax_2.imshow(cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB))
+    ax_2.title.set_text(pic)
 
 fig_2.tight_layout()
 plt.show()
 
 #%%
-#Resizing of 10 random cropped pictures.
+# [SPRINT 2] Cropping 10 random pictures
 fig_3, axs_3 = plt.subplots(nrows = 5, ncols = 2, figsize = (15, 35))
 
 for pic, ax_3 in zip(random_pics, axs_3.ravel()):
 
-    crop_img = src.face_crop(pic, imgs_path, 'image_id',
-                             bbox_col_names, bbox_filtered,
-                             show_bbox = False )
-    
-    resize_crop_img = cv2.resize(crop_img, (300, 300))
-    ax_3.imshow(cv2.cvtColor(resize_crop_img, cv2.COLOR_BGR2RGB))
+    crop_img = src.face_crop(pic, image_file, 'image_id',
+                             bbox_col_names, bbox_generated,
+                             show_bbox = False)
+
+    ax_3.imshow(cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB))
+    ax_3.title.set_text(pic)
 
 fig_3.tight_layout()
+plt.show()
+
+#%%
+# [SPRINT 2] Resizing of 10 random cropped pictures.
+fig_4, axs_4 = plt.subplots(nrows = 5, ncols = 2, figsize = (15, 35))
+
+for pic, ax_4 in zip(random_pics, axs_4.ravel()):
+
+    resize_crop_img = src.face_crop(pic, image_file, 'image_id',
+                             bbox_col_names, bbox_generated,
+                             show_bbox = False,
+                             resize = True)
+    
+    ax_4.imshow(cv2.cvtColor(resize_crop_img, cv2.COLOR_BGR2RGB))
+    ax_4.title.set_text(pic)
+
+fig_4.tight_layout()
 plt.show()
 # %%
