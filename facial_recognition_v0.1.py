3c9cab78a84b01608bae1256375e5e181601e26f
danielmalinovsky <106654319+danielmalinovsky@users.noreply.github.com>
2022-11-14 19:23:32 +0100

    Sprint 4 base - DOWNLOAD BEFORE STARTING YOUR TASK
    All tasks from sprint 3 implemented

--- facial_recognition_v0.1.py
+++ facial_recognition_v0.1.py
@@ -9,6 +9,20 @@ import os
 import tensorflow as tf
 import keras as keras
 from PIL import Image
+
+from itertools import compress
+
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.layers import Dense, Input, concatenate, Flatten, AveragePooling2D, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.utils import plot_model
+from tensorflow import float32
+from tensorflow import stack
+from tensorflow import convert_to_tensor
+
 #import cvlib as cv
 
 
@@ -272,6 +286,8 @@ dist_var_df
 df_joined = identity_filtered.merge(attbs.reset_index().rename(columns = {'index':'image'}), on = 'image')
 df_joined
 
+x = src.balanced_pairs(df_joined, 200, ['Wearing_Necklace', 'Heavy_Makeup', 'Wearing_Lipstick'])
+
 # %% [Sprint 3] Task 3 - Generate balance pairs (PETER)
 
 src.balanced_pairs(df_joined, 10, ['5_o_Clock_Shadow', 'Arched_Eyebrows', 'Attractive'])
@@ -281,7 +297,7 @@ src.balanced_pairs(df_joined, 10, ['5_o_Clock_Shadow', 'Arched_Eyebrows', 'Attra
 
 src.balanced_pairs(df_joined, 15, ['Wearing_Necklace', 'Straight_Hair', 'Blond_Hair'])
 
-# %% [Sprint 3] Task 4 - ResNet 50 with keras application
+# %% [Sprint 3] Task 4 - Resnet 50 with keras application
 
 img_from_array = Image.fromarray(cv2.cvtColor(resize_crop_img, cv2.COLOR_BGR2RGB))
 
@@ -292,16 +308,202 @@ img_resnet
 #ResNet50 model
 model = ResNet50(weights='imagenet')
 
-x = image.img_to_array(img_resnet)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
+x_test = image.img_to_array(img_resnet)
+x_test = np.expand_dims(x, axis=0)
+x_test = preprocess_input(x)
 
-preds = model.predict(x)
+preds = model.predict(x_test)
 print('Predicted:', decode_predictions(preds, top=3)[0])
 
 # %% [Sprint 3] Task 5 - Resnet Binary classification
 
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
 
+# %% Generated list of cropped images using bbox_engine
+
+croplist1 = []
+bbox_generated = pd.DataFrame(columns= ['image_id', 'x_1', 'y_1', 'width', 'height', 'x_end', 'y_end'])
+for i,pic in zip(x.index, x['pic_1']):
+    bbox_coordinates = src.bbox_engine_img_input(pic, './data/Img/img_celeba/') #Dan, please, change
+
+    #Generation of bounding boxes for the pic_1, if the bounding boxes are available.
+    if bbox_coordinates != None:
+        bbox_coordinates['image_id'] = pic
+        bbox_generated = bbox_generated.append(bbox_coordinates, ignore_index = True)
+        startX = bbox_generated[bbox_generated['image_id'] == pic][bbox_col_names['x_start']].values[0]
+        startY = bbox_generated[bbox_generated['image_id'] == pic][bbox_col_names['y_start']].values[0]
+        endX = startX + bbox_generated[bbox_generated['image_id'] == pic][bbox_col_names['width']].values[0]
+        endY = startY + bbox_generated[bbox_generated['image_id'] == pic][bbox_col_names['height']].values[0]
+        img =  cv2.imread('./data/Img/img_celeba/' + pic)
+        crop_img = cv2.resize(img[startY:endY, startX:endX], (224, 224))
+        crop_img = convert_to_tensor(crop_img, dtype=float32)
+        croplist1.append(crop_img)
+    #If the bounding boxes are not available, input 0 array.
+    else:
+        print(pic,'... no bounding boxes detected')
+        zz = np.zeros(shape = (224, 224, 3))
+        zz = convert_to_tensor(zz, dtype=float32)
+        croplist1.append(zz)
+
+croplist2 = []
+bbox_generated = pd.DataFrame(columns= ['image_id', 'x_1', 'y_1', 'width', 'height', 'x_end', 'y_end'])
+for i,pic in zip(x.index, x['pic_2']):
+    bbox_coordinates = src.bbox_engine_img_input(pic, './data/Img/img_celeba/') #Dan, please, change
+
+    #Generation of bounding boxes for the pic_1, if the bounding boxes are available.
+    if bbox_coordinates != None:
+        bbox_coordinates['image_id'] = pic
+        bbox_generated = bbox_generated.append(bbox_coordinates, ignore_index = True)
+        startX = bbox_generated[bbox_generated['image_id'] == pic][bbox_col_names['x_start']].values[0]
+        startY = bbox_generated[bbox_generated['image_id'] == pic][bbox_col_names['y_start']].values[0]
+        endX = startX + bbox_generated[bbox_generated['image_id'] == pic][bbox_col_names['width']].values[0]
+        endY = startY + bbox_generated[bbox_generated['image_id'] == pic][bbox_col_names['height']].values[0]
+        img =  cv2.imread('./data/Img/img_celeba/' + pic)
+        crop_img = cv2.resize(img[startY:endY, startX:endX], (224, 224))
+        crop_img = convert_to_tensor(crop_img, dtype=float32)
+        croplist2.append(crop_img)
+
+    #If the bounding boxes are not available, input 0 array.
+    else:
+        print(pic,'... no bounding boxes detected')
+        zz = np.zeros(shape = (224, 224, 3))
+        zz = convert_to_tensor(zz, dtype=float32)
+        croplist2.append(zz)
+
+print(len(croplist1))
+print(len(croplist2))
+
+# %% Filtering image pairs with bboxes and converting numpy arrays of images into tensorflow objects
+
+#Filtering only those pairs, which have bounding boxes for both pictures.
+inds_to_keep = []
+for i in range(len(croplist1)):
+    if (np.sum(croplist1[i].numpy()) == 0) or (np.sum(croplist2[i].numpy()) == 0):
+        inds_to_keep.append(False)
+    else:
+        inds_to_keep.append(True)
+
+#tf.stack -> convert numpy arrays into tensorflow objects.
+no_classes_1 = len(x.loc[inds_to_keep,'image_id_1'].values)
+no_classes_2 = len(x.loc[inds_to_keep,'image_id_2'].values)
+croplist1 = stack(np.asarray(list(compress(croplist1, inds_to_keep))))
+croplist2 = stack(np.asarray(list(compress(croplist2, inds_to_keep))))
+labels = x.loc[inds_to_keep,'label'].values
+labels = stack(labels)
+
+print(croplist1.shape)
+print(croplist2.shape)
+print(labels.shape)
+
+# %% Preparing branches of the model for binary classification (same, not same)
+
+#The input layers for both pictures
+input_layer1 = Input(shape=(224, 224, 3), name = 'CroppedImages_1')
+input_layer2 = Input(shape=(224, 224, 3), name = 'CroppedImages_2')
+
+#Pooling for both input layers
+x1 = AveragePooling2D(pool_size=150, name = 'AvgPooling2D_1')(input_layer1)
+x2 = AveragePooling2D(pool_size=150, name = 'AvgPooling2D_2')(input_layer2)
+
+#Flatteting both layers
+avgpool2d1 = Flatten(name = 'Flatten_1')(x1)
+avgpool2d2 = Flatten(name = 'Flatten_2')(x2)
+
+#1st hidden layers
+dense_layer1 = Dense(3000, activation="relu", name = 'Dense_1_1')(avgpool2d1)
+dense_layer2 = Dense(3000, activation="relu", name = 'Dense_2_1')(avgpool2d2)
+
+#2nd hidden layers
+dense_layer11 = Dense(4000, activation="relu", name = 'Dense_1_2')(dense_layer1)
+dense_layer22 = Dense(4000, activation="relu", name = 'Dense_2_2')(dense_layer2)
+
+#3rd hidden layers
+dense_layer111 = Dense(5000, activation="relu", name = 'Dense_1_3')(dense_layer11)
+dense_layer222 = Dense(5000, activation="relu", name = 'Dense_2_3')(dense_layer22)
+
+#4th hidden layers
+dense_layer1111 = Dense(10000, activation="relu", name = 'Dense_1_4')(dense_layer111)
+dense_layer2222 = Dense(10000, activation="relu", name = 'Dense_2_4')(dense_layer222)
+
+#Multiclass output layers for both pictures
+y1_output = Dense(units=croplist1.shape[0], activation = 'softmax', name = 'Output_1')(dense_layer1111)
+y2_output = Dense(units=croplist2.shape[0], activation = 'softmax', name = 'Output_2')(dense_layer2222)
+
+#Model initialization for both branches
+model1 = Model(inputs=input_layer1, outputs=y1_output)
+model2 = Model(inputs=input_layer2, outputs=y2_output)
+
+#Merging the outputs into final branch
+combined = concatenate([model1.output, model2.output], name = 'Combined')
+
+#1st hidden layer
+comb_dense = Dense(3000, activation="relu", name = 'CombinedDense_1')(combined)
+
+#2nd hidden layer
+comb_dense1 = Dense(4000, activation="relu", name = 'CombinedDense_2')(comb_dense)
+
+#3rd hidden layer
+comb_dense2 = Dense(5000, activation="relu", name = 'CombinedDense_3')(comb_dense1)
+
+#4th hidden layer
+comb_dense3 = Dense(10000, activation="relu", name = 'CombinedDense_4')(comb_dense2)
+
+#Final output
+comb_output = Dense(1, name='FinalOutput', activation = 'softmax')(comb_dense3)
+
+#Final model
+comb_model = Model(inputs = [model1.input, model2.input], outputs = comb_output)
+
+#Compilation of a new model
+comb_model.compile(optimizer ='sgd', loss = tf.keras.losses.BinaryCrossentropy(), metrics = ['accuracy'])
+
+# %% visualising model
+
+plot_model(comb_model, show_shapes=True)
+
+# %% Using resnet for each branch of the model, and merging model into final layer (binary classification)
+
+#1st branch
+base_model1 = ResNet50(weights='imagenet', input_shape = (224, 224, 3), include_top=False)
+layer1 = base_model1.output
+layer1 = GlobalAveragePooling2D()(layer1)
+predictions1 = Dense(no_classes_1, activation='softmax')(layer1)
+model1 = Model(inputs=base_model1.input, outputs=predictions1)
+
+#2nd branch
+base_model2 = ResNet50(weights='imagenet', input_shape = (224, 224, 3), include_top=False)
+layer2 = base_model2.output
+layer2 = GlobalAveragePooling2D()(layer2)
+predictions2 = Dense(no_classes_2, activation='softmax')(layer2)
+model2 = Model(inputs=base_model2.input, outputs=predictions2)
+
+#Renaming and distinguishing the models' layers.
+for layer in model1.layers :
+    layer._name = layer.name + str('_1')
+for layer in model2.layers :
+    layer._name = layer.name + str('_2')
+
+#Merging and final model
+combo = concatenate([model1.output, model2.output])
+predictions3 = Dense(1, activation='sigmoid')(combo)
+fin_model = Model(inputs = [model1.input, model2.input], outputs = predictions3)
+
+fin_model.summary()
+
+# %% training lart layer of the model (merge into final model for classification is now trained to output only binary classification)
+
+fin_model.compile(optimizer ='sgd', loss = tf.keras.losses.BinaryCrossentropy(), metrics = ['accuracy'])
+
+modelicek = fin_model.fit([croplist1, croplist2], labels,  epochs=5, validation_split=0.2, shuffle = True) 
+
+# %% Prediction of image
+
+predikce = fin_model.predict([croplist1, croplist2])
+
+plt.hist(predikce)
+plt.show()
 
 # %% 
 # [Sprint 3] 
--- src/proprietary_functions.py
+++ src/proprietary_functions.py
@@ -1,7 +1,13 @@
 import cv2
-import cvlib as cv
+#import cvlib as cv
+import src.face_detection as cv
 from matplotlib import pyplot as plt
 import pandas as pd
+from sklearn.metrics import matthews_corrcoef
+import random
+import numpy as np
+import seaborn as sns
+
 
 def face_crop(image_name, image_file, image_id_col_name, bbox_col_names, bbox_df, show_bbox = False, resize = False):
 # Loading Image
@@ -64,6 +70,43 @@ def bbox_engine(image, image_id = '', method = 0, m1_scale_factor = 1.1, m1_min_
             'y_end' : faces[0][1] + faces[0][3]}
     return bbox
 
+def bbox_engine_img_input(pic,image_path, m1_scale_factor = 1.1, m1_min_neighbors = 13):
+    img = cv2.imread(image_path+pic)
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(image = img, scaleFactor = m1_scale_factor, minNeighbors = m1_min_neighbors)
+    
+    #New feature of the function - iteration over the neighbors parameter in case of empty bounding boxes.
+    #Perhaps, we could also perform a hyperparameter tuning in order to choose an optimal number of neighbors which has the lowest validation error.
+    while len(faces) < 1:
+        m1_min_neighbors -= 1
+        faces = face_cascade.detectMultiScale(image = img, scaleFactor = m1_scale_factor, minNeighbors = m1_min_neighbors)
+        if m1_min_neighbors < 0:
+            break
+    
+        #for face in faces:
+            #(startX,startY) = face[0],face[1]
+            #(endX,endY) = face[2],face[3]
+    if len(faces) > 0:
+
+        bbox = {
+    #       "image_id" : image_id,
+            "x_1" : faces[0][0],
+            "y_1" : faces[0][1],
+            "width" : faces[0][2],
+            "height" : faces[0][3],
+            'x_end' : faces[0][0] + faces[0][2],
+            'y_end' : faces[0][1] + faces[0][3]}
+
+        return bbox
+
+bbox_col_names = {
+    'x_start' : 'x_1',
+    'y_start' : 'y_1',
+    'width' : 'width',
+    'height' : 'height',
+    'x_end' : '',
+    'y_end' : ''}
+
 def bbox_verification( bbox_annotation, bbox_generated, image_len):
 
     calc_df = bbox_annotation.merge(bbox_generated, on= "image_id", how='left', suffixes= ['_anno', '_generated'])
@@ -101,3 +144,81 @@ def bbox_show_compare(image_name, image_file, image_id_col_name, bbox_col_names,
 
     fig_1.tight_layout()
     plt.show()
+
+def corr_atrbs(atrbs):
+    dictt = {}
+    atrbs_copy = atrbs.copy()
+
+    for col in atrbs_copy.columns:
+        temp = atrbs_copy.drop(col, axis = 1)
+
+        for col2 in temp.columns:
+            coef = matthews_corrcoef(atrbs_copy[col], temp[col2])
+            dictt[col+'_&_'+col2] = coef
+    
+        atrbs_copy.drop(col, axis = 1, inplace = True)
+
+    corr_df = pd.DataFrame([list(dictt.keys()), list(dictt.values())]).transpose().rename(columns = {0:'combos', 1:'coef'}).sort_values(by = 'coef')
+
+    return corr_df
+
+def corr_matrix(atr_df, plot = False):
+
+    corr_mat = pd.DataFrame(columns = atr_df.columns, index = atr_df.columns)
+
+    for row in corr_mat.index:
+        for col in corr_mat.index:
+            coeff = matthews_corrcoef(atr_df[row], atr_df[col])
+            corr_mat.loc[row, col] = coeff
+
+    if plot:
+        plt.figure(figsize=(20,15))
+        corr_mat = corr_mat.fillna(0.0)
+        mask = np.triu(np.ones_like(corr_mat))
+        cax = sns.heatmap(corr_mat, vmin = -1.0, vmax = 1.0, mask = mask, cmap ='plasma')
+        cax.tick_params(labelsize=10)
+        plt.show()
+        
+    else:
+        return corr_mat
+
+def balanced_pairs(joined_df, n, atrbs):
+
+    pairs_df = pd.DataFrame(columns = ['pic_1', 'pic_2', 'label'])
+
+    joined_df_grouped = joined_df.groupby(atrbs)
+    balanced_df = joined_df_grouped.apply(lambda x: x.sample(joined_df_grouped.size().min()).reset_index(drop=True))
+
+    if n % 2 != 0:
+        adj_list = random.sample([0, 1], k = 2)
+        range_same = int(n/2) + adj_list[0]
+        range_diff = int(n/2) + adj_list[1]
+    else:
+        range_same = n/2
+        range_diff = n/2
+ 
+    for i in range(int(range_same)):
+        random_id_same = random.sample(list(balanced_df['image_id']), k = 1)
+        while balanced_df.loc[balanced_df['image_id'] == random_id_same[0],'image'].shape[0] < 2:
+            random_id_same = random.sample(list(balanced_df['image_id']), k = 1)
+        random_same_pics = random.sample(list(balanced_df.loc[balanced_df['image_id'] == random_id_same[0],'image']), k = 2)
+
+        hehehe = pd.DataFrame({'pic_1': [random_same_pics[0]], 'pic_2': [random_same_pics[1]], 'label':1})
+        pairs_df = pd.concat((pairs_df, hehehe))
+
+    for j in range(int(range_diff)):
+        random_id_same1 = random.sample(list(balanced_df['image_id']), k = 1)
+        random_id_same2 = random.sample(list(balanced_df.loc[balanced_df['image_id'] != random_id_same1[0],'image_id']), k = 1)
+
+        random_same_pic1 = random.sample(list(balanced_df.loc[balanced_df['image_id'] == random_id_same1[0],'image']), k = 1)
+        random_same_pic2 = random.sample(list(balanced_df.loc[balanced_df['image_id'] == random_id_same2[0],'image']), k = 1)
+
+        hahaha = pd.DataFrame({'pic_1': random_same_pic1, 'pic_2': random_same_pic2, 'label': 0})
+        pairs_df = pd.concat((pairs_df, hahaha))
+
+    final_df = pairs_df.merge(joined_df[['image_id', 'image']+atrbs], left_on = 'pic_1', right_on = 'image').\
+                                drop('image', axis=1).\
+                            merge(joined_df[['image_id', 'image']+atrbs], left_on = 'pic_2', right_on = 'image', suffixes= ('_1', '_2')).\
+                                drop('image', axis=1)
+
+    return final_df.iloc[random.sample(list(final_df.index), final_df.shape[0]),:].reset_index(drop = True)
